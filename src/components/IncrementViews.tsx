1e3e7b43df6cdeeb7f152c584002c6cbba3f8c17
Urvesh Bogun <urvesh.bogun@gmail.com>
2025-08-14 19:10:56 +0100

    Revert "Included the rest of the code for IaC"
    This reverts commit d353ffe43603c02c82e876caf6571956e0ffcfcb.

--- src/components/IncrementViews.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-import { useEffect } from 'react';
-
-export default function IncrementView() {
-  useEffect(() => {
-    fetch(process.env.NEXT_PUBLIC_FUNC_URL!).catch(() => {});
-  }, []);
-  return null;
-}
